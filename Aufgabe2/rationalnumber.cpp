9e0199077d7dffe75fe3dd0d33d7a783c6124da4
osym49 <sebastian-bernard@arcor.de>
2014-05-12 17:08:35 +0200

    Aufgabe 2.1 luft durch

--- Aufgabe2/rationalnumber.cpp
+++ Aufgabe2/rationalnumber.cpp
@@ -1,6 +1,69 @@
 #include "rationalnumber.h"
+namespace rn {
 
-RationalNumber::RationalNumber()
+using namespace rn;
+
+bool rn::RationalNumber::isValid()const{
+    if(this->denom() == 0)
+        return false;
+    return true;
+
+}
+
+int Euclid(int a, int b)
 {
+    if(b == 0)
+        return a;
+    else
+        return Euclid(b, a%b);
+}
+
+RationalNumber normalize(RationalNumber rn) {
+    int gcd = Euclid(rn.num(),rn.denom());
+    return RationalNumber(rn.num()/gcd,rn.denom()/gcd);
+}
+
+RationalNumber rn::RationalNumber::operator+(RationalNumber rhs) const{
+     int newnum = num()* rhs.denom() + rhs.num() * denom();
+     int newdenom = denom() * rhs.denom();
+     return normalize(RationalNumber(newnum,newdenom));
+ }
+
+RationalNumber rn::RationalNumber::operator-(RationalNumber rhs) const{
+     int newnum = num()* rhs.denom() - rhs.num() * denom();
+     int newdenom = denom() * rhs.denom();
+     return normalize(RationalNumber(newnum,newdenom));
+ }
+
+RationalNumber rn::RationalNumber::operator*(RationalNumber rhs) const{
+     int newnum = num()*rhs.num();
+     int newdenom = denom() * rhs.denom();
+     return normalize(RationalNumber(newnum,newdenom));
+ }
+RationalNumber rn::RationalNumber::operator-() const {
+   return RationalNumber(-1*num(), denom());
+}
+
+bool rn::RationalNumber::operator==(RationalNumber rhs) const{
+   RationalNumber n1 = normalize(RationalNumber(this->num(),this->denom()));
+   RationalNumber n2 = normalize(rhs);
+
+    if(n1.num() == n2.num() && n1.denom() == n2.denom())
+        return true;
+    return false;
+ }
+RationalNumber rn::RationalNumber::inverse() const{
+    return RationalNumber(denom(),num());
+}
+RationalNumber rn::RationalNumber::operator/(RationalNumber rhs) const{
+    return normalize(rhs * RationalNumber(this->denom(),this->num()));
+}
+bool rn::RationalNumber::operator <(RationalNumber rn) const{
+    if((float)this->num()/(float)this->denom() < (float)rn.num()/(float)rn.denom())
+        return true;
+    return false;
+
+}
+
 
 }
--- Aufgabe2/rationalnumber.h
+++ Aufgabe2/rationalnumber.h
@@ -1,10 +1,28 @@
 #ifndef RATIONALNUMBER_H
 #define RATIONALNUMBER_H
-
+namespace rn{
 class RationalNumber
 {
+    int m_numerator, m_denominator;
 public:
-    RationalNumber();
-};
+    RationalNumber(int numerator = 0,int denominator = 1):
+        m_numerator(numerator), m_denominator(denominator)
+    {
+    }
+    int num() const {return m_numerator;}
+    int denom() const {return m_denominator;}
+    bool isValid() const;
+    RationalNumber inverse() const;
+    RationalNumber operator+(RationalNumber rhs) const;
+    RationalNumber operator-(RationalNumber rhs) const;
+    RationalNumber operator-() const;
+    RationalNumber operator*(RationalNumber rhs) const;
+    RationalNumber operator/(RationalNumber rhs) const;
+    bool operator==(RationalNumber rhs) const;
+    RationalNumber operator-();
+    bool operator<(RationalNumber rhs) const;
 
+
+};
+}
 #endif // RATIONALNUMBER_H
